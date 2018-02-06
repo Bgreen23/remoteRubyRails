a6ee9d5d00595d2102c6eee35fbf3cc3e95e80f8
Brandon <31549535+Bgreen23@users.noreply.github.com>
2018-02-05 22:25:18 -0500

    noticed some errors in the code, applying fixes

--- app/models/animes.rb
+++ app/models/animes.rb
@@ -1,5 +1,5 @@
 class Anime < ApplicationRecord
    has_many :microposts
-   validates FILL_IN, presence: true 
-   validates FILL_IN, presence: true
+   validates name:, presence: true
+   validates creator:, presence: true
 end
