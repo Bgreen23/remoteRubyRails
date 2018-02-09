9e709e37a1245f4d239edb438d82270826a7838e
Brandon <31549535+Bgreen23@users.noreply.github.com>
2018-02-09 14:14:00 -0500

    still attempting to get heroku to function

--- app/models/animes.rb
+++ app/models/animes.rb
@@ -2,5 +2,5 @@ class Anime < ApplicationRecord
    has_many :microposts
    #Next two lines  are the exact same thing just different syntax
    validates(:name, {:presence => true})
-   validates :creator, presence: true
+   validates (:creator, {:presence => true})
 end
